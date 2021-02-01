5143bd480f6ea9024fb2e6c78a8a21bb7990e6ea
dmtrby <dmitry.mahliui@gmail.com>
2021-02-01 23:34:58 +0300

    feat: smth

--- src/components/Header/components/YellowBanner/YellowBanner.js
+++ src/components/Header/components/YellowBanner/YellowBanner.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { Row, Col } from 'react-flexbox-grid';
 
-import yellowBannerStyles from './yellow-banner.module.scss';
+import s from './yellow-banner.module.scss';
 
 const YellowBanner = () => {
   return (
     <a href="#">
-<div className={yellowBannerStyles.banner}>
-    <Row center="xs">
-    <Col xs={12} md={6}>
-      
+      <div className={s.banner}>
         <div className="banner__title">
           <strong>Maximise your local area wellbeing</strong>
         </div>
-        </Col>
-        <Col xs={12} md={6}>
         <div className="banner__label">
           tailored support for local authorities
         </div>
-        </Col>
-      
-      </Row>
       </div>
     </a>
-    
   )
 };
 
